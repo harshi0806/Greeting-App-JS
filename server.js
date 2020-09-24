01a2ba39a631caebc037dcf4f789b0939ef1c9fb
harshi0806 <harshitashrivatav121@gmail.com>
2020-09-24 21:27:51 +0530

    FEAT: Added server.js for setting up the web server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// create express app
+const greetingApp = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+greetingApp.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+greetingApp.use(bodyParser.json())
+
+// define a simple route
+greetingApp.get('/', (req, res) => {
+    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+});
+
+// listen for requests
+greetingApp.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
