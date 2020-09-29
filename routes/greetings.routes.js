b733e0a81ba73c26faa1451698bf04c0fb2245d9
harshi0806 <harshitashrivatav121@gmail.com>
2020-09-29 17:45:52 +0530

    FIX: For exporting routes for app

--- routes/greetings.routes.js
+++ routes/greetings.routes.js
@@ -1,22 +1,19 @@
-const express = require('express')
-const router = express.Router()
+module.exports = (app) => {
+    //import controller
+    const greetingController = require('../controllers/greetings.controller.js')
 
-//import controller
-const greetingController = require('../controllers/greetings.controller.js')
+    //Create a new Greeting
+    app.post('/greetings', greetingController.create)
+    
+    //Retrieve all Greetings
+    app.get('/greetings', greetingController.findAll)
+    
+    // Retrieve a single Greeting with greetingId
+    app.get('/greetings/:greetingId', greetingController.findOne);
 
-//Create a new Greeting
-router.post('/greetings', greetingController.create)
- 
-//Retrieve all Greetings
-router.get('/greetings', greetingController.findAll)
- 
-// Retrieve a single Greeting with greetingId
-router.get('/greetings/:greetingId', greetingController.findOne);
+    // Update a Greeting with greetingId
+    app.put('/greetings/:greetingId', greetingController.update);
 
-// Update a Greeting with greetingId
-router.put('/greetings/:greetingId', greetingController.update);
-
-// Delete a Greeting with greetingId
-router.delete('/greetings/:greetingId', greetingController.delete);
-
-module.exports = router
+    // Delete a Greeting with greetingId
+    app.delete('/greetings/:greetingId', greetingController.delete);
+}
